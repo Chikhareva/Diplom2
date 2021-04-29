af9871326521fd70237471572a3d751bf7dd2b15
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-04-29 20:43:20 +0300

    add project

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,99 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class DataHelper {private static Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    public static String getFirstCardNumber() {
+        return "4444 4444 4444 4441";
+    }
+
+    public static String getFirstCardExpectedStatus() {
+        return "APPROVED";
+    }
+
+    public static String getSecondCardNumber() {
+        return "4444 4444 4444 4442";
+    }
+
+    public static String getSecondCardExpectedStatus() {
+        return "DECLINED";
+    }
+
+    public static String getCardNumberDifferent() {
+        return faker.business().creditCardNumber();
+    }
+
+    public static String getCardNumberEmpty() {
+        return "                   ";
+    }
+
+    public static String getCardNumberZero() {
+        return "0000 0000 0000 0000";
+    }
+
+    public static String getValidMonth() {
+        return "01";
+    }
+
+    public static String getInvalidMonth() {
+        return "13";
+    }
+
+    public static String getEmptyMonth() {
+        return "  ";
+    }
+
+    public static String getZeroMonth() {
+        return "00";
+    }
+
+    public static String getValidYear() {
+        return "24";
+    }
+
+    public static String getInvalidYear() {
+        return "14";
+    }
+
+    public static String getEmptyYear() {
+        return "  ";
+    }
+
+    public static String getValidOwner() {
+        return faker.name().fullName();
+    }
+
+    public static String getInvalidOwner() {
+        return "Петр(. 123";
+    }
+
+    public static String getEmptyOwner() {
+        return "  ";
+    }
+
+    public static String getLongOwner() {
+        return "Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин Владимир Ленин";
+    }
+
+    public static String getValidCvs() {
+        return "456";
+    }
+
+    public static String getInvalidCvs() {
+        return "12";
+    }
+
+    public static String getEmptyCvs() {
+        return "  ";
+    }
+
+    public static String getZeroCvs() {
+        return "000";
+    }
+}
---
+++ src/test/java/ru/netology/test/BuyWithCreditTest.java
@@ -0,0 +1,67 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.DatabaseHelper;
+import ru.netology.page.MainPage;
+import ru.netology.page.PaymentPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuyWithCreditTest {
+    MainPage mainPage = new MainPage();
+    PaymentPage paymentPage = new PaymentPage();
+
+    @BeforeEach
+    void shouldCleanDataBaseAndOpenWeb() {
+        DatabaseHelper.cleanDataBase();
+        open("http://localhost:8080", MainPage.class);
+        mainPage.buyWithCredit();
+
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Test
+    void shouldApproveFirstCard() {
+        val cardNumber = DataHelper.getFirstCardNumber();
+        val month = DataHelper.getValidMonth();
+        val year = DataHelper.getValidYear();
+        val owner = DataHelper.getValidOwner();
+        val cvs = DataHelper.getValidCvs();
+        paymentPage.fillOutFields(cardNumber, month, year, owner, cvs);
+        paymentPage.expectApprovalFromBank();
+        val expected = DataHelper.getFirstCardExpectedStatus();
+        val actual = DatabaseHelper.getStatusPaymentWithCredit();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRejectSecondCard() {
+        val cardNumber = DataHelper.getSecondCardNumber();
+        val month = DataHelper.getValidMonth();
+        val year = DataHelper.getValidYear();
+        val owner = DataHelper.getValidOwner();
+        val cvs = DataHelper.getValidCvs();
+        paymentPage.fillOutFields(cardNumber, month, year, owner, cvs);
+        paymentPage.expectRejectionFromBank();
+        val expected = DataHelper.getSecondCardExpectedStatus();
+        val actual = DatabaseHelper.getStatusPaymentWithCredit();
+        assertEquals(expected, actual);
+    }
+}
